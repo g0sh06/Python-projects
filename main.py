99ae680520b9a0c8233f21e65d4a355e4dc88a0f
g0sh06 <s.atanasov51@gmail.com>
2025-01-08 20:56:49 +0100

    working line

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ class Window:
         self.root = tk.Tk()
         self.root.title("My Maze soler")
         self.is_running = False
-        self.canvas = tk.Canvas(self.root, width = width, height = height)
+        self.canvas = tk.Canvas(self.root, width = width, height = height, bg="white")
         self.canvas.pack(expand = True, fill = 'both')
         self.root.protocol("WM_DELETE_WINDOW", self.close)
 
@@ -20,9 +20,36 @@ class Window:
             self.redraw()
 
     def close(self):
-        self.is_running = False       
+        self.is_running = False
+
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.canvas, fill_color)      
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, point1, point2):
+        self.point1 = point1
+        self.point2 = point2
+
+    def draw(self, canvas, fill_color="black"):
+        canvas.create_line(
+            self.point1.x, self.point1.y, self.point2.x, self.point2.y, fill=fill_color, width=2
+        )
+
+                   
+
 
 if __name__ == "__main__":
     app = Window(800,600)
+    #example line, which is working
+    point1 = Point(300,300)
+    point2 = Point(500, 500)
+    line = Line(point1=point1, point2=point2)
+    app.draw_line(line=line)
+
     app.wait_for_close()            
         
